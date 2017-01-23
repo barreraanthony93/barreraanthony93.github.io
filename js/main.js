14d873a0040f4346cc2068742dd2f83cd23bf93f
barreraanthony93 <Anthony.Barrera@d300.org>
2017-01-23 10:32:55 -0600

    Website Test

---
+++ js/main.js
@@ -0,0 +1,267 @@
+
+var controller = new ScrollMagic.Controller();
+
+
+//CoverLoader
+var 
+    rec1 = $(".rec1"),   
+    rec2 = $(".rec2"),
+    spinner =$(".spinner"),
+    symbol = $(".symbol"),
+    symbolMobile = $(".symbolMobile"),
+    overlay = $(".CoverSlides")
+
+var CoverLoader = new TimelineMax()
+
+    .to(spinner, 1, {opacity: 0, ease:Power4.easeOut, delay:5})
+    .to (symbol, 1, {opacity:1, ease:Power1.easeIn},"-=3")
+    .to (symbol, 1, {scale:4, ease:Power1.easeOut})
+    .to (symbol, .5, {opacity:0, ease:Power1.easeOut})
+    .to (symbolMobile, 1, {opacity:1, ease:Power1.easeIn},"-=5")
+    .to (symbolMobile, 1, {scale:6, ease:Power1.easeOut}, "-=1")
+    .to (symbolMobile, .5, {opacity:0, ease:Power1.easeOut})
+    .to(rec1, 1, {x: "-100%", ease:Power4.easeInOut})
+    .to(rec2, 1, {x: "100%", ease:Power4.easeInOut},'-=1')
+    .set(overlay, { zIndex: -99})
+
+    
+    //Services
+
+    var 
+        servHead = $("#servicesHeader"),
+        servIcon = $("#iconServices .service-box")
+        
+        
+    var servicesTl = new TimelineMax()
+        .from(servHead, 1, {opacity: 0, y: -100})
+        .from(servIcon, 1, {y:100, opacity:0,  ease:Bounce.easeNone},.5,'-=1')
+        
+    
+     var sceneServ = new ScrollMagic.Scene({
+        triggerElement: "#tabpanel",
+        triggerHook: .1
+    })
+        .setTween(servicesTl)
+        .addIndicators({name: "serv"})
+        .addTo(controller);
+    
+
+    //animation  
+   
+    var
+        head = $("#head"),
+        subhead = $("#subhead"),
+        play = $('#playBtn');
+
+    var header = new TimelineMax()
+        .to(head, 1, {opacity: 1})
+        .to(subhead, 1, {opacity: 1}, '-=.5')
+        .to(play, 1, {opacity:1}, '-=.5')
+  
+        var scene = new ScrollMagic.Scene({
+            triggerElement: "#trigger1",
+            
+           
+        })
+        .setTween(header)
+        .addIndicators({name: "header"}) 
+        .addTo(controller);
+
+//Parallax
+ 
+
+    var slideParallaxScene = new ScrollMagic.Scene({
+        triggerElement: "#parallax",
+        triggerHook: .7,
+        duration:'130%'
+    })
+    .setTween(TweenMax.from('.bcg-parallax', 1, {yPercent: -30, force3D:true, ease:Power0.easeNone}))
+    .addIndicators({name: "parallax"})
+    .addTo(controller);
+    
+    //crecimineto
+
+   var 
+        growh2 = $("#growh2"),
+        growp = $("#growp"), 
+        growbtn = $("#growbtn"), 
+        grow = $("#grow img");
+       
+    var creci = new TimelineMax()
+        .to(growh2, 2, {opacity: 1})
+        .to(growp, 2, {opacity: 1}, '-=1')
+        .to(growbtn, 2, {opacity: 1}, '-=2')
+        .staggerFrom (grow, 1.5, {opacity: 0, x: -200, ease:Power4.easeOut},.4, '-=2')
+
+        var scene2 = new ScrollMagic.Scene({
+            triggerElement: "#triggerCrecimiento",
+            triggerHook: 0
+        })
+        .setTween(creci)
+        .addIndicators({name: "conectate"}) 
+        .addTo(controller);
+
+/*
+//conenctate
+    var
+        team = $(".Team12"),
+        team2 = $(".Team2")
+    
+    var teamScene = new TimelineMax()
+        .staggerFrom(team, 1, {x:-1500, zIndex:9},.5)
+    .staggerFrom(team2, 1, {x:1500},.5, "-.1")
+    
+    var sceneTeam = new ScrollMagic.Scene({
+        triggerElement: "#conectate"
+    })
+    .setTween(teamScene)
+    .addIndicators({name:"team"})
+    .addTo(controller);
+*/
+
+
+
+
+
+
+
+
+
+
+(function($){
+        
+        var 
+            cross = $('#cross'),
+            equal = $('#equal'),
+            bapt = $('#bapt'),
+            rock = $('#rock'),
+            grave = $('#grave'),
+            equal2 = $('#equal2'),
+            baptIn = $('#baptIn'),
+            resurec = $('#resurec'),
+            resurecGround = $('#resurecGround'),
+            resurecCloud = $('#resurecCloud'),
+            equal3 = $('#equal3'),
+            baptOut = $('#baptOut'),
+            restart = $('#restartBtn'),
+            pausebtn =$("#previousBtn"),
+            playbtn =$("#nextBtn"),
+           
+        tl = new TimelineLite();
+        
+        TweenLite.set(restart, {autoAlpha: 0});
+        
+        
+    tl 
+        .to(head, 1, {opacity: 0})
+        .to(subhead, 1, {opacity: 0}, '-=1')
+        .from(cross, 2, {x: 400, scale:0, delay: .5})
+        .to(equal, 2, {opacity: 1, scale:1})
+        .from(bapt, 2, {x: -400, scale:0, opacity:0})
+        .add('exit')
+        .to(cross, 2, {autoAlpha: 0}, 'exit')
+        .to(equal, 2, {autoAlpha: 0}, 'exit')
+       
+        .from(grave, 2, { scale:0})
+         .to(equal2, 2, {opacity: 1, scale:1})
+        .to(bapt, 2, {autoAlpha: 0})
+        .from(baptIn, 2, {autoAlpha:0}, '-=2')
+        .add('exit2')
+        .to(grave, 2, {autoAlpha: 0}, 'exit2')
+        .to(equal2, 2, {autoAlpha: 0}, 'exit2')
+        
+       
+         .from(resurec, 2, { scale:0})
+        .add('resurecIn')
+        .from(resurecGround, 2, { scale:0}, '-=2')
+        .from(resurecCloud, 2, { scale:0}, '-=2')
+        .add('resurec')
+        .to(resurec, 3, { opacity:0, y:-550}, 'resurec')
+        .to(resurecGround, 2, {opacity:.2, y:50}, 'resurec')
+        .to(resurecCloud, 2, {opacity:.2, y:50}, 'resurec')
+        .to(equal3, 2, {opacity: 1, scale:1})
+        .to(baptIn, 2, {autoAlpha: 0})
+        .from(baptOut, 2, {autoAlpha: 0}, '-=2')
+        .add('exit3')
+        .to(resurec, 2, {autoAlpha: 0}, 'exit3')
+        .to(resurecGround, 2, {opacity: 0, y:50}, 'exit3')
+        .to(resurecCloud, 2, {opacity:0 , y:50}, 'exit3')
+        .to(equal3, 2, {autoAlpha: 0}, 'exit3')
+        .to(baptOut, 2, {autoAlpha: 0}, 'exit3')
+        
+        .to(pausebtn, 1, {opacity:0}, 'exit3')
+        .to(playbtn, 1, {opacity:0}, 'exit3')
+        .to(restart, 2, {autoAlpha: 1})
+        
+        
+      
+      
+    tl.pause();
+        
+        $('#playBtn').on('click', function(){
+            TweenLite.to(play, .5, {autoAlpha: 0});
+            TweenLite.to(pausebtn, .5, {opacity:1});
+            TweenLite.to(playbtn, .5, {opacity:1});
+            tl.play();
+        });
+     
+    $('#previousBtn').on('click', function(){
+            
+            tl.pause();
+        });
+     $('#nextBtn').on('click', function(){
+            tl.play();
+        });
+        
+        
+      
+        
+          $('#restartBtn').on('click', function(){
+              TweenLite.set(restart, {autoAlpha: 0});
+            tl.restart();
+        });
+    
+    
+    
+    ////SUB PAGES/////
+    
+    var 
+        h1 = $(".brand-heading"),
+        hsub = $(".intro-text"),
+        btn = $(".btn-circle"),
+        scenein = $(".sceneIn"),
+        socialTop = $(".socialTop i")
+    
+    var SubpageInto = new TimelineMax()
+    
+    .from(h1, 2, {x: 300, opacity: 0})
+    .from(hsub, 2, {x: -300, opacity: 0})
+    .staggerFrom(socialTop, 1, {opacity:0, y:50, ease:Power0.easeNone, delay:5}, .5, '-=1')
+    .from(btn, 1, {y: 10, opacity: 0, ease:Power0.easeNone}, '-=1.5')
+   
+   
+    
+    ///Predicador
+    
+      var
+        featureImg = $(".featurette-image"),
+        featureh2 = $(".featurette-heading"),
+        featurep = $(".lead")  
+          
+    var featureIn = new TimelineMax()
+        .from(featureh2, 1, {opacity:0, x:-100})
+        .from(featurep, 1, {opacity:0, y:100}, '-=1')
+        .from(featureImg, 1, {scale: 0, ease:Bounce.easeOut})
+       
+    
+    var scenetab = new ScrollMagic.Scene({
+        triggerElement: "#info"
+        
+    })
+        .setTween(featureIn)
+        .addIndicators({name: "featureIn"})
+        .addTo(controller);
+    
+ 
+        
+    })(jQuery);
---
+++ js/playlistcustomize.js
@@ -0,0 +1,430 @@
+$(document).ready(function() {
+
+	new jPlayerPlaylist({
+		jPlayer: "#jquery_jplayer_2",
+		cssSelectorAncestor: "#jp_container_2"
+	}, [
+		{
+			title:"El Mas Grande Mandamiento",
+			mp3:"OLD/AMARASADIOSJUDIO.mp3"
+		},
+        {
+			title:"Amaras A Tu Prójimo",
+			mp3:"OLD/AMARASATUPROJIMOJUDIO.mp3"
+		},
+         {
+			title:"Paradoja Judia",
+			mp3:"OLD/Paradoja.mp3"
+		},
+         {
+			title:"Amen Emunah",
+			mp3:"OLD/EmunahAmen.mp3"
+		},
+		
+	], {
+		swfPath: "../../dist/jplayer",
+		supplied: "oga, mp3",
+		wmode: "window",
+		useStateClassSkin: true,
+		autoBlur: false,
+		smoothPlayBar: true,
+		keyEnabled: true
+	});
+    
+    
+    new jPlayerPlaylist({
+		jPlayer: "#jquery_jplayer_3",
+		cssSelectorAncestor: "#jp_container_3"
+	}, [
+	
+		{
+			title:"Introducción",
+			mp3:"OLD/CORINTIOSUNO.mp3"
+		},
+        {
+			title:"Perfectamente Unidos",
+			mp3:"OLD/CORINTIOSDOS.mp3"
+		},
+        {
+			title:"Sabiduría vs. Insensatez",
+			mp3:"OLD/CORINTIOSTRES.mp3"
+		},
+        {
+			title:"Sabiduría",
+			mp3:"OLD/CORINTIOSCUATRO.mp3"
+		},
+        {
+			title:"La Producción de la Sabiduría",
+			mp3:"OLD/CORINTIOSCINCO.mp3"
+		},
+        {
+			title:"Sobreedificación Espiritual",
+			mp3:"OLD/CORINTIOSSEIS.mp3"
+		},
+        {
+			title:"Llegar a ser sabios",
+			mp3:"OLD/CORINTIOSSIETE.mp3"
+		},
+        {
+			title:"Las Parábolas de Pablo",
+			mp3:"OLD/parabolasdepablo.mp3"
+		},
+        {
+			title:"Fornicación en Corinto",
+			mp3:"OLD/CORINTIOSOCHO.mp3"
+		},
+        {
+			title:"Después de la Muerte Física",
+			mp3:"OLD/Despuesdelamuertefisica.mp3"
+		},
+        /*{
+			title:"La Pascua en la Iglesia",
+			mp3:"OLD/"
+		},*/
+	
+	], {
+		swfPath: "../../dist/jplayer",
+		supplied: "oga, mp3",
+		wmode: "window",
+		useStateClassSkin: true,
+		autoBlur: false,
+		smoothPlayBar: true,
+		keyEnabled: true
+	});
+    
+    
+     new jPlayerPlaylist({
+		jPlayer: "#jquery_jplayer_4",
+		cssSelectorAncestor: "#jp_container_4"
+	}, [
+	
+		  {
+			title:"Israel e Iglesia",
+			mp3:"audio/TTI/TTI Introduccion.m4a"
+		},
+          {
+			title:"Israel e Iglesia Inicio",
+			m4a:"audio/TTI/TTI I.m4a"
+		},
+          {
+			title:"Primogenitura Israel",
+			mp3:"audio/TTI/TTI.mp3"
+		},
+          {
+			title:"La Vida de Jacob",
+			mp3:"audio/TTI/TTITres.mp3"
+		},
+          {
+			title:"La Vida de Jacob II",
+			mp3:"audio/TTI/TTI IV.mp3"
+		},
+          {
+			title:"La Vida de Jacob III",
+			mp3:"audio/TTI/TTI V.mp3"
+		},
+          {
+			title:"La Vida de Jose",
+			mp3:"audio/TTI/TTI VI.mp3"
+		},
+          /*{
+			title:"La Vida de Jose II",
+			mp3:"OLD/TTI I.m4a"
+		},*/
+          /*{
+			title:"La Vida de Jose III",
+			mp3:"OLD/"
+		},*/
+          {
+			title:"La Vida de Jose IV",
+			mp3:"audio/TTI/TTI X.mp3"
+		},
+          {
+			title:"La Vida en Egipto",
+			mp3:"audio/TTI/TTI XII.mp3"
+		},
+          {
+			title:"Egipto II Moises I",
+			mp3:"audio/TTI/TTI XIII.mp3"
+		},
+          {
+			title:"Moises II",
+			mp3:"audio/TTI/TTI XIII Moises II Parte.mp3"
+		},
+          {
+			title:"Moises III",
+			mp3:"audio/TTI/TTI XIV.mp3"
+		},
+          /*{
+			title:"Moises IV",
+			mp3:"OLD/"
+		},
+          {
+			title:"Moises V",
+			mp3:"OLD/"
+		},*/
+          {
+			title:"Moises VI",
+			mp3:"audio/TTI/TTI XVII.mp3"
+		},
+          {
+			title:"Moises VII",
+			mp3:"audio/TTI/TTI XVIII.mp3"
+		},
+          {
+			title:"Moises VIII",
+			mp3:"audio/TTI/TTI XIX.mp3"
+		},
+          /*{
+			title:"Moises IX",
+			mp3:"OLD/"
+		},*/
+         
+	
+	], {
+		swfPath: "../../dist/jplayer",
+		supplied: "oga, mp3, m4a",
+		wmode: "window",
+		useStateClassSkin: true,
+		autoBlur: false,
+		smoothPlayBar: true,
+		keyEnabled: true
+	});
+    
+     new jPlayerPlaylist({
+		jPlayer: "#jquery_jplayer_5",
+		cssSelectorAncestor: "#jp_container_5"
+	}, [
+	
+		{
+			title:"La Parábola de la mostaza y Levadura",
+			mp3:"OLD/ElGranodeMostaza.mp3"
+		},
+         {
+			title:"La Parábola del Tesoro y La Perla",
+			mp3:"OLD/DS300027.mp3"
+		},
+         {
+			title:"La Parábola de la Red",
+			mp3:"OLD/DS300035.mp3"
+		},
+         {
+			title:"La Parábola del Candil",
+			mp3:"OLD/LaParaboladelCandilUno.mp3"
+		},
+         {
+			title:"Gadareno",
+			mp3:"OLD/GADARENO.mp3"
+		},
+         {
+			title:"Dos Ciego",
+			mp3:"OLD/DosCiegos.mp3"
+		},
+         {
+			title:"Un Mudo y La Labor",
+			mp3:"OLD/UnMudoyLaLabor.mp3"
+		},
+	
+	], {
+		swfPath: "../../dist/jplayer",
+		supplied: "oga, mp3",
+		wmode: "window",
+		useStateClassSkin: true,
+		autoBlur: false,
+		smoothPlayBar: true,
+		keyEnabled: true
+	});
+     new jPlayerPlaylist({
+		jPlayer: "#jquery_jplayer_6",
+		cssSelectorAncestor: "#jp_container_6"
+	}, [
+	
+		{
+			title:"Your Face",
+			mp3:"http://www.jplayer.org/audio/mp3/TSP-05-Your_face.mp3",
+			oga:"http://www.jplayer.org/audio/ogg/TSP-05-Your_face.ogg"
+		},
+	
+	], {
+		swfPath: "../../dist/jplayer",
+		supplied: "oga, mp3",
+		wmode: "window",
+		useStateClassSkin: true,
+		autoBlur: false,
+		smoothPlayBar: true,
+		keyEnabled: true
+	});
+     new jPlayerPlaylist({
+		jPlayer: "#jquery_jplayer_7",
+		cssSelectorAncestor: "#jp_container_7"
+	}, [
+	
+		{
+			title:"Your Face",
+			mp3:"http://www.jplayer.org/audio/mp3/TSP-05-Your_face.mp3",
+			oga:"http://www.jplayer.org/audio/ogg/TSP-05-Your_face.ogg"
+		},
+	
+	], {
+		swfPath: "../../dist/jplayer",
+		supplied: "oga, mp3",
+		wmode: "window",
+		useStateClassSkin: true,
+		autoBlur: false,
+		smoothPlayBar: true,
+		keyEnabled: true
+	});
+     new jPlayerPlaylist({
+		jPlayer: "#jquery_jplayer_8",
+		cssSelectorAncestor: "#jp_container_8"
+	}, [
+	
+		{
+			title:"Your Face",
+			mp3:"http://www.jplayer.org/audio/mp3/TSP-05-Your_face.mp3",
+			oga:"http://www.jplayer.org/audio/ogg/TSP-05-Your_face.ogg"
+		},
+	
+	], {
+		swfPath: "../../dist/jplayer",
+		supplied: "oga, mp3",
+		wmode: "window",
+		useStateClassSkin: true,
+		autoBlur: false,
+		smoothPlayBar: true,
+		keyEnabled: true
+	});
+     new jPlayerPlaylist({
+		jPlayer: "#jquery_jplayer_9",
+		cssSelectorAncestor: "#jp_container_9"
+	}, [
+	
+		{
+			title:"Your Face",
+			mp3:"http://www.jplayer.org/audio/mp3/TSP-05-Your_face.mp3",
+			oga:"http://www.jplayer.org/audio/ogg/TSP-05-Your_face.ogg"
+		},
+	
+	], {
+		swfPath: "../../dist/jplayer",
+		supplied: "oga, mp3",
+		wmode: "window",
+		useStateClassSkin: true,
+		autoBlur: false,
+		smoothPlayBar: true,
+		keyEnabled: true
+	});
+     new jPlayerPlaylist({
+		jPlayer: "#jquery_jplayer_10",
+		cssSelectorAncestor: "#jp_container_10"
+	}, [
+	
+		{
+			title:"Your Face",
+			mp3:"http://www.jplayer.org/audio/mp3/TSP-05-Your_face.mp3",
+			oga:"http://www.jplayer.org/audio/ogg/TSP-05-Your_face.ogg"
+		},
+	
+	], {
+		swfPath: "../../dist/jplayer",
+		supplied: "oga, mp3",
+		wmode: "window",
+		useStateClassSkin: true,
+		autoBlur: false,
+		smoothPlayBar: true,
+		keyEnabled: true
+	});
+     new jPlayerPlaylist({
+		jPlayer: "#jquery_jplayer_11",
+		cssSelectorAncestor: "#jp_container_11"
+	}, [
+	
+		{
+			title:"Your Face",
+			mp3:"http://www.jplayer.org/audio/mp3/TSP-05-Your_face.mp3",
+			oga:"http://www.jplayer.org/audio/ogg/TSP-05-Your_face.ogg"
+		},
+	
+	], {
+		swfPath: "../../dist/jplayer",
+		supplied: "oga, mp3",
+		wmode: "window",
+		useStateClassSkin: true,
+		autoBlur: false,
+		smoothPlayBar: true,
+		keyEnabled: true
+	});
+     new jPlayerPlaylist({
+		jPlayer: "#jquery_jplayer_12",
+		cssSelectorAncestor: "#jp_container_12"
+	}, [
+	
+		{
+			title:"Your Face",
+			mp3:"http://www.jplayer.org/audio/mp3/TSP-05-Your_face.mp3",
+			oga:"http://www.jplayer.org/audio/ogg/TSP-05-Your_face.ogg"
+		},
+	
+	], {
+		swfPath: "../../dist/jplayer",
+		supplied: "oga, mp3",
+		wmode: "window",
+		useStateClassSkin: true,
+		autoBlur: false,
+		smoothPlayBar: true,
+		keyEnabled: true
+	});
+    
+     new jPlayerPlaylist({
+		jPlayer: "#jquery_jplayer_13",
+		cssSelectorAncestor: "#jp_container_13"
+	}, [
+	
+		{
+			title:"Your Face",
+			mp3:"http://www.jplayer.org/audio/mp3/TSP-05-Your_face.mp3",
+			oga:"http://www.jplayer.org/audio/ogg/TSP-05-Your_face.ogg"
+		},
+	
+	], {
+		swfPath: "../../dist/jplayer",
+		supplied: "oga, mp3",
+		wmode: "window",
+		useStateClassSkin: true,
+		autoBlur: false,
+		smoothPlayBar: true,
+		keyEnabled: true
+	});
+    
+     new jPlayerPlaylist({
+		jPlayer: "#jquery_jplayer_himno",
+		cssSelectorAncestor: "#jp_container_himno"
+	}, [
+	
+		{
+			title:"himno",
+			mp3:"http://www.jplayer.org/audio/mp3/TSP-05-Your_face.mp3",
+			oga:"http://www.jplayer.org/audio/ogg/TSP-05-Your_face.ogg",
+            
+		},
+         {
+			title:"himno",
+			mp3:"http://www.jplayer.org/audio/mp3/TSP-05-Your_face.mp3",
+			oga:"http://www.jplayer.org/audio/ogg/TSP-05-Your_face.ogg"
+		},
+         {
+			title:"himno",
+			mp3:"http://www.jplayer.org/audio/mp3/TSP-05-Your_face.mp3",
+			oga:"http://www.jplayer.org/audio/ogg/TSP-05-Your_face.ogg"
+		},
+	
+	], {
+		swfPath: "../../dist/jplayer",
+		supplied: "oga, mp3",
+		wmode: "window",
+		useStateClassSkin: true,
+		autoBlur: false,
+		smoothPlayBar: true,
+		keyEnabled: true
+	});
+    
+});
