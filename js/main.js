450df1663dcc7c4c8f7b0369e4e5d6d3525a9fbb
barreraanthony93 <Anthony.Barrera@d300.org>
2017-01-25 09:08:35 -0600

    update

--- js/main.js
+++ js/main.js
@@ -83,10 +83,10 @@ var CoverLoader = new TimelineMax()
        brain = $(".headBrain");;
        
     var creci = new TimelineMax()
+        .from(brain, 1, {scale: 0, ease:Bounce.easeIn}, '-=.5')
         .from(growh2, 1, {x:-1000})
         .from(growp, 1, {x:-1000}, '-=.5')
         .from(growbtn, 1, {x:1000}, '-=.5')
-        .from(brain, 1, {scale: 0, ease:Bounce.easeIn}, '-=.5')
 
         var scene2 = new ScrollMagic.Scene({
             triggerElement: "#triggerCrecimiento",
@@ -105,7 +105,7 @@ var CoverLoader = new TimelineMax()
     
     var teamScene = new TimelineMax()
         .from(team, 2, {opacity:0})
-        .from(teamAll, 2, {opacity:0, y:100})
+        .from(teamAll, 2, {opacity:0, y:100}, "-=1")
     
     var sceneTeam = new ScrollMagic.Scene({
         triggerElement: "#conectate",
@@ -250,15 +250,15 @@ var CoverLoader = new TimelineMax()
         btn = $(".btn-circle"),
         scenein = $(".sceneIn"),
         menu = $("#menu-toggle"),
-        brandImg =$("#brand"),
+        brandImg =$(".navbar-brand"),
         socialTop = $(".socialTop i")
     
     var SubpageInto = new TimelineMax()
     
     .from(h1, 2, {x: 300, opacity: 0, delay:2})
-    .from(brand, 2, {opacity: 0, delay:2}, '-=1')
+    .from(brandImg, 2, {opacity: 0, delay:2}, '-=1')
     .from(menu, 2, {opacity: 0}, '-=2')
-    .from(hsub, 2, {x: -300, opacity: 0})
+    .from(hsub, 2, {x: -300, opacity: 0},"-=3")
     .from(socialTop, 1, {opacity:0, y:50, ease:Power0.easeNone, delay:1})
     .to(btn, 1, {y: 10, opacity: 1, ease:Power0.easeNone},'-=1')
    
@@ -278,7 +278,8 @@ var CoverLoader = new TimelineMax()
        
     
     var scenetab = new ScrollMagic.Scene({
-        triggerElement: "#info"
+        triggerElement: "#bio",
+        triggerHook: .5,
         
     })
         .setTween(featureIn)
