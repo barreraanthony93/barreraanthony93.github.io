2a78698b91337d9f8a57bee22be577e88eceae62
Anthony Barrera <barreraanthony93@yahoo.com>
2019-02-19 22:25:39 -0600

    udpate

---
+++ precache-manifest.f002b8527ec4129f54c3814709d194c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e47421f9b8cec2661620743c53475c8d",
+    "url": "/static/media/Poppins-Bold.e47421f9.otf"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d3d0b3f1c317f449d084",
+    "url": "/static/js/main.d3d0b3f1.chunk.js"
+  },
+  {
+    "revision": "7827d07cd9b16ad5131c",
+    "url": "/static/js/2.7827d07c.chunk.js"
+  },
+  {
+    "revision": "d3d0b3f1c317f449d084",
+    "url": "/static/css/main.127dc9d5.chunk.css"
+  },
+  {
+    "revision": "5d6fe7a269f99cebb31f8127f7950487",
+    "url": "/index.html"
+  }
+];
