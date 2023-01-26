2cdcf4c7f4a8cd3a10f354446721fd4dd3aa0172
Anthony Barrera <barreraanthony93@yahoo.com>
2023-01-25 22:25:35 -0600

    Added Grid layout

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,36 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import About from './About'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Projects from './Projects'
+import Resume from './Resume'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/about",
+    element: <About/>,
+  },
+  {
+    path: "/projects",
+    element: <Projects/>,
+  },
+  {
+    path: "/Resume",
+    element: <Resume/>,
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode> 
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
